9b167218cc3c4184b860c44f05ed0217aeae7f99
Steven Day <steve@mysociety.org>
2017-06-20 21:10:08 +0100

    Add GEOS and GDAL library paths to settings

--- aadf/aadf/settings.py
+++ aadf/aadf/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 import os
 import dj_database_url
 
+GEOS_LIBRARY_PATH = "/app/.heroku/vendor/lib/libgeos_c.so"
+GDAL_LIBRARY_PATH = "/app/.heroku/vendor/lib/libgdal.so"
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
