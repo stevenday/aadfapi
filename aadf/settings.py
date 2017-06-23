1408eeaba4b8e153c5f57c8a358ae8050480ff30
Steven Day <steve@mysociety.org>
2017-06-23 09:02:51 +0100

    Make sure db settings always come from

--- aadf/settings.py
+++ aadf/settings.py
@@ -79,16 +79,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'aadf.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -113,9 +103,11 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-# Update database configuration with $DATABASE_URL.
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+# Database
+# https://docs.djangoproject.com/en/1.10/ref/settings/#databases
+# Set database configuration from $DATABASE_URL.
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config(conn_max_age=500)
 DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
