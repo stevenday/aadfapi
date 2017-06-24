ebbc90f9c98d7e4cc0535eda82340c1f5856a8a2
Steven Day <steve@mysociety.org>
2017-06-24 23:16:39 +0100

    Swap our basic list view for Django Rest Framework's

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,29 @@
 """api URL Configuration"""
-from django.conf.urls import url
+from django.conf.urls import url, include
 
-from .views import CountListView
+from rest_framework import routers, serializers, viewsets
+
+from .models import Count
+
+
+# Serializers define the API representation.
+class CountSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Count
+        fields = '__all__'
+
+
+# ViewSets define the view behavior.
+class CountViewSet(viewsets.ModelViewSet):
+    queryset = Count.objects.all()
+    serializer_class = CountSerializer
+
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'counts', CountViewSet)
+
 
 urlpatterns = [
-    url(r'^$', CountListView.as_view(), name='counts')
+    url(r'^', include(router.urls))
 ]
--- api/views.py
+++ api/views.py
@@ -1,18 +1,2 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
-from django.core import serializers
-from django.http import JsonResponse
-from django.views.generic import ListView
-
-from .models import Count
-
-
-class CountListView(ListView):
-    model = Count
-    context_object_name = 'counts'
-
-    def get(self, request, *args, **kwargs):
-        queryset = self.get_queryset()
-        data = serializers.serialize("json", queryset)
-        return JsonResponse(data, status=200, safe=False)
